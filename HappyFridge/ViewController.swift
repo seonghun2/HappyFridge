b23e4c0f9860df13038f7f30219a87d5c5f4df42
Wantaek <wtchoi@happysmile.kr>
2022-10-30 09:26:57 +0900

    Initial Commit

---
+++ HappyFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyFridge
+//
+//  Created by 해피스마일 on 2022/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
