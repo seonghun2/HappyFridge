a8bf3b49222f8d8958b9566986bd7b23494737fb
seonghun2 <tjdgnsok2@naver.com>
2022-10-30 11:42:43 +0900

    test

--- HappyFridge/ViewController.swift
+++ HappyFridge/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("test")
     }
     
     override func viewDidAppear(_ animated: Bool) {
