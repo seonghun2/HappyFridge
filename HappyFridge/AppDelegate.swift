c03c0a1bcadc6df5542325ac7a22744be7a71c3c
LeeChungWoo <Ios.lllccww@gmail.com>
2022-11-01 17:56:46 +0900

    Add: FireStore 라이브러리 추가 및 닉네임 중복체크 추가

--- HappyFridge/AppDelegate.swift
+++ HappyFridge/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: "a109cf78cb61d71d128dfe47692a543b")
+        FirebaseApp.configure()
         return true
     }
 
