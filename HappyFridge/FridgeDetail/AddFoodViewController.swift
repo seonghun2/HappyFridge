0661bbb957deeff6363d76361b307c190ce35e61
LeeChungWoo <ios.lllccww@gmail.com>
2022-11-17 10:21:04 +0900

    물품추가 UI작업 완료
    - 물품추가 db작업 진행 예정
    - 필수값 어떤걸로 할지 정해서 버튼 활성화 진행예정

--- HappyFridge/FridgeDetail/AddFoodViewController.swift
+++ HappyFridge/FridgeDetail/AddFoodViewController.swift
@@ -7,61 +7,59 @@
 
 import UIKit
 
-//class AddFoodViewController: UIViewController {
-//
-//    @IBOutlet weak var foodNameTextField: UITextField!
-//    @IBOutlet weak var foodCountTextField: UITextField!
-//    @IBOutlet weak var alarmDayTextField: UITextField!
-//
-//    @IBOutlet weak var addButton: UIButton!
-//
-//    var foodCount = 0
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        addButton.layer.cornerRadius = 8
-//        foodCountTextField.layer.cornerRadius = 4
-//        alarmDayTextField.layer.cornerRadius = 4
-//        foodCountTextField.text = String(foodCount)
-//
-//        startButtonActivate(activate: false)
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        self.view.endEditing(true)
-//    }
-//
-//
-//    @IBAction func minusAction(_ sender: Any) {
-//        foodCount = Int(foodCountTextField.text!)!
-//        if foodCount == 0 {
-//            return
-//        }else {
-//            foodCount -= 1
-//            foodCountTextField.text = String(foodCount)
-//        }
-//    }
-//    @IBAction func plusAction(_ sender: Any) {
-//        foodCount = Int(foodCountTextField.text!)!
-//        foodCount += 1
-//        foodCountTextField.text = String(foodCount)
-//    }
-//    @IBAction func addAction(_ sender: Any) {
-//
-//    }
-//
-//    func startButtonActivate(activate:Bool) {
-//        if activate {
-//            self.addButton.isEnabled = true
-//            self.addButton.backgroundColor =  colorLiteral(red: 0.03921568627, green: 0.5882352941, blue: 0.1254901961, alpha: 1)
-//
-//        } else {
-//            self.addButton.isEnabled = false
-//            self.addButton.backgroundColor = .gray
-//        }
-//    }
-//
-//}
+class AddFoodViewController: UIViewController {
 
-//git 테스트
+    @IBOutlet weak var foodNameTextField: UITextField!
+    @IBOutlet weak var foodCountTextField: UITextField!
+    @IBOutlet weak var alarmDayTextField: UITextField!
+
+    @IBOutlet weak var addButton: UIButton!
+
+    var foodCount = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addButton.layer.cornerRadius = 8
+        foodCountTextField.layer.cornerRadius = 4
+        alarmDayTextField.layer.cornerRadius = 4
+        foodCountTextField.text = String(foodCount)
+
+        startButtonActivate(activate: false)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+
+    @IBAction func minusAction(_ sender: Any) {
+        foodCount = Int(foodCountTextField.text!)!
+        if foodCount == 0 {
+            return
+        }else {
+            foodCount -= 1
+            foodCountTextField.text = String(foodCount)
+        }
+    }
+    @IBAction func plusAction(_ sender: Any) {
+        foodCount = Int(foodCountTextField.text!)!
+        foodCount += 1
+        foodCountTextField.text = String(foodCount)
+    }
+    @IBAction func addAction(_ sender: Any) {
+
+    }
+
+    func startButtonActivate(activate:Bool) {
+        if activate {
+            self.addButton.isEnabled = true
+            self.addButton.backgroundColor = #colorLiteral(red: 0.03921568627, green: 0.5882352941, blue: 0.1254901961, alpha: 1)
+
+        } else {
+            self.addButton.isEnabled = false
+            self.addButton.backgroundColor = .gray
+        }
+    }
+
+}
